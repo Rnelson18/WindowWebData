5038a2520931ade0e31871abbcd46061d8a5804f
ryannelson <r3nelson@student.bridgew.edu>
2021-11-09 10:45:44 -0500

    end of tues

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module bsu.comp152.example.windowwebdata {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.net.http;
 
     opens bsu.comp152.example.windowwebdata to javafx.fxml;
     exports bsu.comp152.example.windowwebdata;
